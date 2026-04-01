cce5d71ec538a397bddc6df1d76ceb0b8050e177
hansonhua <hanson.hua903@gmail.com>
2026-04-01 01:58:08 -0500

    test run

--- frontend/src/api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = "http://localhost:8000";
-
-export const searchDocs = async (query) => {
-  const res = await axios.get(`${API}/search?q=${query}`);
-  return res.data;
-};
